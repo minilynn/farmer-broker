530b60f74d7e3488abd0c1ba6328d7e4ce5ede89
Lynn <Lynn@O20106615879601>
2018-06-12 12:37:46 +0800

    integrate mybatis

--- src/main/java/com/gul/farmerbroker/goods/dos/GoodsRepositoryImpl.java
+++ src/main/java/com/gul/farmerbroker/goods/dos/GoodsRepositoryImpl.java
@@ -5,26 +5,35 @@ package com.gul.farmerbroker.goods.dos;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.gul.farmerbroker.goods.domain.Goods;
+import com.gul.farmerbroker.goods.mapper.GoodsMapper;
 
 /**
  * @author Lynn
- *
  */
 @Repository
-public class GoodsRepositoryImpl implements IGoodsRepository {
-
+public class GoodsRepositoryImpl implements IGoodsRepository, InitializingBean {
+	@Autowired
+	private SqlSession sqlSessOion;
+	private GoodsMapper gmapper;
+	
 	@Override
 	public List<Goods> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return gmapper.findAll();
 	}
 
 	@Override
 	public void save(Goods goods) {
-		// TODO Auto-generated method stub
+		gmapper.insert(goods);
+	}
 
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		gmapper = sqlSessOion.getMapper(GoodsMapper.class);
 	}
 }
